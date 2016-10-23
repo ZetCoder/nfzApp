4e2c8097f3a31ca7424f347f4379e74adbc42597
dakwasniewski@gmail.com <dakwasniewski@gmail.com>
2016-10-23 16:53:28 +0200

    some simple validation on couple of fields added and watchGroup usage

--- js/app.controller.js
+++ js/app.controller.js
@@ -10,9 +10,9 @@
       // If we need add some logic 
       $scope.headItemsTur = ["Punkty - Turośń", "Kwota - Turośń", "Prywatni - Turośń", "Suma"];
       $scope.headItemsBia = ["Punkty - Białystok", "Kwota - Białystok", "Prywatni - Białystok", "Suma"];
-      $scope.pointsTur = 0;
+      $scope.pointsTur;
       $scope.pointsBia = 0;
-      $scope.privateMoneyTur = 0; 
+      $scope.privateMoneyTur; 
       $scope.privateMoneyBia = 0;
       $scope.newPrivateMoneyTur = 0;
       $scope.newPrivateMoneyBia = 0;
@@ -25,8 +25,12 @@
       $scope.sumTur = 0;
       $scope.sumBia = 0;
       $scope.sum = 0;
-      $scope.regFloat = /[0-9]|[.]/;
-      $scope.regInteger = /[0-9]/;
+
+      $scope.validMessage = {
+        validPointsMessage: "Please enter any points value",
+        validValueMessage: "Please enter any private money",
+        validRequiredMessage: "Please fill required field"
+      };
 
       /* .toFixedNumber() is highly recommended to use instead of .toFixed() 
       which converts numbers to strings for some purpose, please visit 
@@ -38,44 +42,33 @@
         return + (Math.round(this * pow) / pow);
       }
 
-      $scope.allowKey = function ($event, reg) {
-        var accessKeys = $event.key !== "Enter" &&
-                         $event.key !== "Backspace" && 
-                         $event.key !== "Shift" && 
-                         $event.key !== "Tab";
-
-        if(!reg.test($event.key) && accessKeys) {
-            $event.returnValue = false;
-        }
-      }
-
       $scope.submit = function () {
           $scope.sumTur = 0;
           $scope.sumBia = 0;
           
           if ($scope.pointsTur) {
-              $scope.pointsTur = parseFloat($scope.pointsTur); //typed vale on input field is returning it as string, so it need to be parsed to number
-              $scope.newPointsTur = $scope.newPointsTur + $scope.pointsTur;
-              $scope.newValueTur = ($scope.newPointsTur * 0.38).toFixedNumber(2);
-              $scope.pointsTur = ""; //empty string assigning here is needed to clear input field
+              $scope.pointsTur = parseFloat($scope.pointsTur); //typed value on input field is returning it as string, so it need to be parsed to number
+              //$scope.newPointsTur = $scope.newPointsTur + $scope.pointsTur;
+              //$scope.newValueTur = ($scope.newPointsTur * 0.38).toFixedNumber(2);
+              //$scope.pointsTur = ""; //empty string assigning here is needed to clear input field
           }
           
           if ($scope.pointsBia) {
-              $scope.pointsBia = parseFloat($scope.pointsBia); //typed vale on input field is returning it as string, so it need to be parsed to number
+              $scope.pointsBia = parseFloat($scope.pointsBia); //typed value on input field is returning it as string, so it need to be parsed to number
               $scope.newPointsBia = $scope.newPointsBia + $scope.pointsBia;
               $scope.newValueBia = ($scope.newPointsBia * 0.322).toFixedNumber(2);
               $scope.pointsBia = ""; //empty string assigning here is needed to clear input field
           }
           
           if ($scope.privateMoneyTur) {
-              $scope.privateMoneyTur = parseFloat($scope.privateMoneyTur); //typed vale on input field is returning it as string, so it need to be parsed to number
+              $scope.privateMoneyTur = parseFloat($scope.privateMoneyTur); //typed value on input field is returning it as string, so it need to be parsed to number
               $scope.newPrivateMoneyTur = $scope.newPrivateMoneyTur + $scope.privateMoneyTur;
               $scope.newPrivateTur = $scope.newPrivateTur + $scope.privateMoneyTur * 0.45;
               $scope.privateMoneyTur = ""; //empty string assigning here is needed to clear input field
           }
           
           if ($scope.privateMoneyBia) {
-              $scope.privateMoneyBia = parseFloat($scope.privateMoneyBia); //typed vale on input field is returning it as string, so it need to be parsed to number
+              $scope.privateMoneyBia = parseFloat($scope.privateMoneyBia); //typed value on input field is returning it as string, so it need to be parsed to number
               $scope.newPrivateMoneyBia = $scope.newPrivateMoneyBia + $scope.privateMoneyBia;
               $scope.newPrivateBia = $scope.newPrivateBia + $scope.privateMoneyBia * 0.40;
               $scope.privateMoneyBia = ""; //empty string assigning here is needed to clear input field
@@ -83,9 +76,15 @@
           
           $scope.sumTur = ($scope.sumTur + $scope.newPrivateTur + $scope.newValueTur).toFixedNumber(2);
           $scope.sumBia = ($scope.sumBia + $scope.newPrivateBia + $scope.newValueBia).toFixedNumber(2);
+
+          if(!$scope.userForm.$invalid) {
+            alert("Brawo - poprawnie wypełniłeś formularz!");
+          }
       }
 
-      $scope.$watchGroup(["sumTur", "sumBia"], function() {
+      $scope.$watchGroup(["sumTur", "sumBia", "pointsTur", "newPointsTur"], function() {
+        $scope.newPointsTur = $scope.newPointsTur + $scope.pointsTur;
+        $scope.newValueTur = ($scope.newPointsTur * 0.38).toFixedNumber(2);
         $scope.sum = ($scope.sumTur + $scope.sumBia).toFixedNumber(2);
       });
     }
